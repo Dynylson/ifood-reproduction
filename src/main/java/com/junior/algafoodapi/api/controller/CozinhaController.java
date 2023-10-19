0fb9316d0f8a673960cd2b1ca6e67725c843c72f
Dynylson Junior <dynylsonjr@gmail.com>
2023-10-19 18:33:07 -0300

    Controller de Cozinha

--- src/main/java/com/junior/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/junior/algafoodapi/api/controller/CozinhaController.java
@@ -2,12 +2,16 @@ package com.junior.algafoodapi.api.controller;
 
 import com.junior.algafoodapi.domain.model.Cozinha;
 import com.junior.algafoodapi.domain.repository.CozinhaRepository;
+import com.junior.algafoodapi.domain.service.CadastroCozinhaService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -16,9 +20,62 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
+
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        Optional<Cozinha> cozinha = cozinhaRepository.findById(id);
+
+        if (cozinha.isPresent()) {
+            return ResponseEntity.ok(cozinha.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cadastroCozinhaService.salvar(cozinha);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+        Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(id);
+
+        if (cozinhaAtual.isPresent()) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+
+            cozinhaRepository.save(cozinhaAtual.get());
+
+            return ResponseEntity.ok(cozinhaAtual.get());
+
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+        try {
+            Optional<Cozinha> cozinha = cozinhaRepository.findById(id);
+
+            if (cozinha.isPresent()) {
+                cozinhaRepository.delete(cozinha.get());
+
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
 }
---
+++ src/main/java/com/junior/algafoodapi/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,18 @@
+package com.junior.algafoodapi.domain.service;
+
+import com.junior.algafoodapi.domain.model.Cozinha;
+import com.junior.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.save(cozinha);
+    }
+
+}
