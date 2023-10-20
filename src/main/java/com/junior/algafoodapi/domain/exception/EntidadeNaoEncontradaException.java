ded1aa18ed61bb0cf98123077f44e352fba2dc4a
Dynylson Junior <dynylsonjr@gmail.com>
2023-10-19 21:42:22 -0300

    Exceptions personalizadas

---
+++ src/main/java/com/junior/algafoodapi/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package com.junior.algafoodapi.domain.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+
+}
